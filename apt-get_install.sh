92adaa19ab1f21f5179fbfa46814f3b3f299aeb7
git4xuan <PXZ377@bham.ac.uk>
2015-03-11 20:15:04 +0800

    apt-get
    将apt-get的东西全部放在一个脚本中，相关的配置文件放在files文件夹中。

---
+++ apt-get_install.sh
@@ -0,0 +1,13 @@
+###Install deluge and web-UI , folder-where
+##shell
+
+###Install NodeQurey  remember Auth Key
+
+###Install nginx and add special-web-index
+#wget --no****
+
+###Install fuse sshfs  mkdir
+
+###Install btsync(for private files? or you can use rcp or other way)
+
+###install bind-tools iftop and others
--- init.sh
+++ init.sh
@@ -10,16 +10,4 @@
 
 ###Install shadowsocks and supervisor config-file local-start.
 
-###Install deluge and web-UI , folder-where
-##shell
-
-###Install NodeQurey  remember Auth Key
-
-###Install nginx and add special-web-index
-#wget --no****
-
-###Install fuse sshfs  mkdir
-
-###Install btsync(for private files? or you can use rcp or other way)
-
-###install bind-tools iftop and others
+###Using apt-get install
