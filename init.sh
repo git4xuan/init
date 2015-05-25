c012f80737963ce965409c0628a7b7b7f4ccc5db
mingxuan <root@fengidea.com>
2015-05-25 13:46:40 +0800

    脚本

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #based on Ubuntu
 ###Using apt-get install
+sudo  apt-get install -y git
+git clone http://github.com/git4xuan/init.git
+cd init
 sudo apt-get install -y supervisor deluge*
 
 chmod +x start/*.sh
