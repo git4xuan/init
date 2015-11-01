2c1e4d7e815ce761a0132c3c95c2dc5fc4a7f0f5
mingxuan <root@fengidea.com>
2015-11-02 03:38:16 +0800

    init  a little changes

--- init-zip.sh
+++ init-zip.sh
@@ -1,11 +1,11 @@
 
 #echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list
-
+# wget https://raw.githubusercontent.com/git4xuan/init/master/init-zip.sh && chmod +x init-zip.sh && sh init-zip.sh
 #apt-add-repository http://backports.debian.org/debian-backports
-
+apt-get update
 apt-get install -y unzip wget
 
 wget https://github.com/git4xuan/init/archive/master.zip && unzip master.zip && cd init-master && chmod +x init.sh && ./init.sh
-
+cd ~
 rm master.zip
 rm init-zip.sh
