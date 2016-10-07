068fc5483785f9906be660539d5c2fdd56acc596
rasp <git4xuan@gmail.com>
2016-10-07 14:21:56 +0000

    fdt download

---
+++ start/fdt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fdt_addr="http://monalisa.cern.ch/FDT/lib/fdt.jar"
+
+apt-get install --force-yes -y openjdk-7-jdk
+
+wget -O fdt.jar  -N --no-check-certificate $fdt_addr  && mv fdt.jar /usr/local/fdt.jar
+
+cp ../files/fdtServer.sh /root  && chmod +x /root/fdtServer.sh
+
+
+
