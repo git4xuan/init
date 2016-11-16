64398c5041a9ea08b883dad0fefb4f19868a649f
rasp <git4xuan@gmail.com>
2016-11-16 21:52:53 +0000

    temp upload

---
+++ unused/new_relic.sh
@@ -0,0 +1,7 @@
+echo deb http://apt.newrelic.com/debian/ newrelic non-free >> /etc/apt/sources.list.d/newrelic.list
+wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
+apt-get update
+apt-get install newrelic-sysmond
+nrsysmond-config --set license_key=972d5cd80e7d288accd5cacb5b0669706701014b
+/etc/init.d/newrelic-sysmond start
+
