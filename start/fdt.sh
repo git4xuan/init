4cc66bfbac7ef4b172ae9ba9e535ee9d6fc8ccbb
mingxuan <git4xuan@users.noreply.github.com>
2018-02-02 20:01:24 +0800

    Update fdt.sh
    change url to cdn.
    change source to default jdk.

--- start/fdt.sh
+++ start/fdt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-fdt_addr="http://monalisa.cern.ch/FDT/lib/fdt.jar"
+fdt_addr="http://oy2v5m0ma.bkt.clouddn.com/fdt.jar"
 
-apt-get install --force-yes -y openjdk-7-jdk
+apt-get install -y default-jdk
 
 wget -O fdt.jar  -N --no-check-certificate $fdt_addr  && mv fdt.jar /usr/local/fdt.jar
 
